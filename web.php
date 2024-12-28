ff1d434906e0d265703c50fd13f5966ec2fea209
alitusher1 <mohammed.ali.tusher@gmail.com>
2024-12-28 23:50:44 +0600

    Add files via upload

---
+++ web.php
@@ -0,0 +1,60 @@
+<?php
+ 
+
+use App\Http\Controllers\{
+   TodoListController,
+   TaskController,
+   ReminderController,
+   HabitController,
+   IdeaController,
+   WeeklyReflectionController,
+   ProfileController
+};
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\FullCalenderController;
+
+Route::get('fullcalender', [FullCalenderController::class, 'index']);
+Route::post('fullcalenderAjax', [FullCalenderController::class, 'ajax']);
+Route::resource('fullcalender', FullCalenderController::class);
+Route::get('/', function () {
+   return view('welcome');
+})->name('welcome');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard', [
+            'recentTasks' => Auth::user()->tasks()->latest()->take(3)->get(),
+            'todaysHabits' => Auth::user()->habits()->get(),
+            'upcomingReminders' => Auth::user()->reminders()->latest()->take(3)->get()
+        ]);
+    })->name('dashboard');
+
+
+
+
+
+    Route::resource('tasks', TaskController::class);
+
+
+   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   Route::get('/todo-lists/archived', [TodoListController::class, 'archived'])->name('todo-lists.archived');
+   Route::post('/todo-lists/{todoList}/archive', [TodoListController::class, 'archive'])->name('todo-lists.archive');
+   Route::post('/todo-lists/{todoList}/restore', [TodoListController::class, 'restore'])->name('todo-lists.restore');
+   Route::resource('todo-lists', TodoListController::class);
+   Route::post('/todo-lists/{todoList}/tasks', [TaskController::class, 'store'])->name('tasks.store');
+   Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+   Route::post('/tasks/{task}/toggle', [TaskController::class, 'toggleComplete'])->name('tasks.toggle');
+   Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+   Route::post('/reminders/{reminder}/snooze', [ReminderController::class, 'snooze'])->name('reminders.snooze');
+   Route::resource('reminders', ReminderController::class);
+   Route::post('/habits/{habit}/log', [HabitController::class, 'logCompletion'])->name('habits.log');
+   Route::resource('habits', HabitController::class);
+   Route::resource('ideas', IdeaController::class);
+   Route::resource('reflections', WeeklyReflectionController::class);
+});
+require __DIR__.'/auth.php';
+
